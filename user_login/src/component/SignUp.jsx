cb03bb832b1118acee05df1b89e2fdd00798b8bc
manish2588 <manishkc258@gmail.com>
2025-01-06 16:08:17 +0545

    Added

--- user_login/src/component/SignUp.jsx
+++ user_login/src/component/SignUp.jsx
@@ -37,6 +37,9 @@ const SignUp = () => {
       toast.error(error.message, {
         position: "top-center",
       });
+    }
+  };
+
   return (
     <div className="bg-white p-6 rounded shadow-md w-80 mx-auto mt-10">
       <ToastContainer />
