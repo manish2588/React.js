6afa2ea9c2a626af6f1bcf0384a850e300e93b3b
manish2588 <manishkc258@gmail.com>
2025-01-26 21:44:06 +0545

    Added

--- animation/src/App.jsx
+++ animation/src/App.jsx
@@ -1,11 +1,13 @@
 
 import ReplacingDiv from "./component/ReplacingDiv";
+import BasicAnimation from "./framerMotion/BasicAnimation";
 
 
 function App() {
   return (
     <div>
       <ReplacingDiv/>
+      <BasicAnimation/>
     </div>
   )
 }
---
+++ animation/src/framerMotion/BasicAnimation.jsx
@@ -0,0 +1,14 @@
+function BasicAnimation() {
+  return (
+    <>
+      <div>
+        <div>Div 1</div>
+        <div>Div 2</div>
+        <div>Div 3</div>
+        <div>Div 4</div>
+      </div>
+    </>
+  );
+}
+
+export default BasicAnimation;
