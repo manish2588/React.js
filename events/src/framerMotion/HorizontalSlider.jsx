7843d3a94df1b1c7d4db91d6222c8a311e433386
manish2588 <manishkc258@gmail.com>
2025-01-26 22:58:31 +0545

    Added

--- events/src/framerMotion/HorizontalSlider.jsx
+++ events/src/framerMotion/HorizontalSlider.jsx
@@ -1,8 +1,119 @@
-
+import { motion } from "framer-motion"
 
 function HorizontalSlider() {
   return (
-    <div>HorizontalSlider</div>
+   <>
+   <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+   <div className="mx-auto w-96 grid place-content-center h-32 relative ">
+    <h1 className="relative z-0"> Scroll to show me</h1>
+    <motion.div className="absolute z-10 bg-green-500 top-0 w-full h-full "
+    initial={{opacity:1,
+        
+    }}
+    whileInView={{opacity:0,
+        
+    }}
+   
+    transition={{duration:1}}
+    viewport={{amount:"all"}}
+    ></motion.div>
+   </div>
+   <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+      <p className="w-96">
+        In grammar, an article is any member of a class of dedicated words that
+        are used with noun phrases to mark the identifiability of the referents
+        of the noun phrases. The category of articles constitutes a part of
+        speech. In English, both  and  are articles, which combine
+        with nouns to form noun phrases. Articles typically specify the
+        grammatical definiteness of the noun phrase, but in many languages, they
+        carry additional grammatical information such as gender, number, and
+        case. Articles are part of a broader category called determiners, which
+        also include demonstratives, possessive determiners, and quantifiers. In
+        linguistic interlinear glossing, articles are abbreviated as ART.
+      </p>
+   </>
   )
 }
 
